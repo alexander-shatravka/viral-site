2e2373b3b79660a09d868bf0f3bad0c2614507c8
Indiel <kori.yanagi@gmail.com>
2019-03-22 14:29:25 +0200

    new

--- index.php
+++ index.php
@@ -442,41 +442,112 @@
                     </div>
                 </div>
                 <!-- blogs info -->
-                <div class="info blogs">
-                    <div class="img-frame">
-                        <div class="txt-holder wow fadeInUp" data-wow-delay="0.2s">
-                            <div class="images">
-                                <img src="http://placehold.it/511x438" alt="image description">
-                                <span class="sub-caption">by Leonard, 1 week ago</span>
+                <section class="commonSection chooseUs">
+                    <!-- <div class="container"> -->
+                        <div class="row">
+                            <div class="col-lg-12 text-center">
+                                <h4 class="sub_title">our benefits</h4>
+                                <h2 class="sec_title">why choose us</h2>
+                                <p class="sec_desc">
+                                    We are committed to providing our customers with exceptional service while<br> offering our employees the best training.
+                                </p>
                             </div>
-                            <div class="txts">
-                                <div class="txt-box">
-                                    <h3>We Do The Best Design</h3>
-                                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa  officia deserunt mollitid est laborum. Sed ut perspiciatis unde qui omnis iste natus error sit voluptatem.</p>
-                                    <a href="blog-article" class="continue"><span>CONTINUE</span> <i class="icon-right-thin"></i></a>
+                        </div>
+                        <div id="tabs">
+                            <div class="row">
+                                <div class="col-lg-12 col-sm-12">
+                                    <ul class="chooseUs_title">
+                                        <li class="active"><a href="#tes_tab_1" data-toggle="tab" aria-expanded="true">Latest technology</a></li>
+                                        <li class=""><a href="#tes_tab_2" data-toggle="tab" aria-expanded="false">awesome support</a></li>
+                                        <li class=""><a href="#tes_tab_3" data-toggle="tab" aria-expanded="false">1 click demo install</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="tab-content clearfix">
+                                    <div class="tab-pane fade active in" id="tes_tab_1">
+                                        <div class="col-lg-7 col-sm-7 col-md-7">
+                                            <div class="wh_choose">
+                                                <p>
+                                                    There are many variations of passages of lorem ipsum available, but the majority have 
+                                                    suffered alteration in some form, by injected humour, or randomised words 
+                                                    which don't look even slightly believable. If you are going to use a 
+                                                    passage of Lorem Ipsum, you need to be sure there isn't anything
+                                                    embarrassing hidden in the middle of text. 
+                                                </p>
+                                                <ul>
+                                                    <li><i class="fa fa-check-square"></i>Self-contained, state-of-the-art time clock</li>
+                                                    <li><i class="fa fa-check-square"></i>Scalability of up to 500 employees per time clock</li>
+                                                    <li><i class="fa fa-check-square"></i>The ability to connect up to 32 time clocks</li>
+                                                    <li><i class="fa fa-check-square"></i>Employee self-enrollment</li>
+                                                    <li><i class="fa fa-check-square"></i>Payroll integration</li>
+                                                    <li><i class="fa fa-check-square"></i>Built-in backup camera to verify failed punches</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-5 col-sm-5 col-md-5">
+                                            <div class="chose_img">
+                                                <img src="images/home_1/c1.jpg" alt="">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane fade" id="tes_tab_2">
+                                        <div class="col-lg-5 col-sm-5 col-md-5">
+                                            <div class="chose_img">
+                                                <img src="images/home_1/c2.jpg" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-7 col-sm-7 col-md-7">
+                                            <div class="wh_choose">
+                                                <p>
+                                                    There are many variations of passages of lorem ipsum available, but the majority have 
+                                                    suffered alteration in some form, by injected humour, or randomised words 
+                                                    which don't look even slightly believable. If you are going to use a 
+                                                    passage of Lorem Ipsum, you need to be sure there isn't anything
+                                                    embarrassing hidden in the middle of text. 
+                                                </p>
+                                                <ul>
+                                                    <li><i class="fa fa-check-square"></i>Self-contained, state-of-the-art time clock</li>
+                                                    <li><i class="fa fa-check-square"></i>Scalability of up to 500 employees per time clock</li>
+                                                    <li><i class="fa fa-check-square"></i>The ability to connect up to 32 time clocks</li>
+                                                    <li><i class="fa fa-check-square"></i>Employee self-enrollment</li>
+                                                    <li><i class="fa fa-check-square"></i>Payroll integration</li>
+                                                    <li><i class="fa fa-check-square"></i>Built-in backup camera to verify failed punches</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane fade" id="tes_tab_3">
+                                        <div class="col-lg-7 col-sm-7 col-md-7">
+                                            <div class="wh_choose">
+                                                <p>
+                                                    There are many variations of passages of lorem ipsum available, but the majority have 
+                                                    suffered alteration in some form, by injected humour, or randomised words 
+                                                    which don't look even slightly believable. If you are going to use a 
+                                                    passage of Lorem Ipsum, you need to be sure there isn't anything
+                                                    embarrassing hidden in the middle of text. 
+                                                </p>
+                                                <ul>
+                                                    <li><i class="fa fa-check-square"></i>Self-contained, state-of-the-art time clock</li>
+                                                    <li><i class="fa fa-check-square"></i>Scalability of up to 500 employees per time clock</li>
+                                                    <li><i class="fa fa-check-square"></i>The ability to connect up to 32 time clocks</li>
+                                                    <li><i class="fa fa-check-square"></i>Employee self-enrollment</li>
+                                                    <li><i class="fa fa-check-square"></i>Payroll integration</li>
+                                                    <li><i class="fa fa-check-square"></i>Built-in backup camera to verify failed punches</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-5 col-sm-5 col-md-5">
+                                            <div class="chose_img">
+                                                <img src="images/home_1/c3.jpg" alt="">
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="txt-holder right wow fadeInUp" data-wow-delay="0.2s">
-                            <div class="txts">
-                                <div class="txt-box">
-                                    <h3>Friendly For All Devices</h3>
-                                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa  officia deserunt mollitid est laborum. Sed ut perspiciatis unde qui omnis iste natus error sit voluptatem.</p>
-                                    <a href="#" class="continue"><span>CONTINUE</span> <i class="icon-right-thin"></i></a>
-                                </div>
-                            </div>
-                            <div class="images">
-                                <span class="sub-caption">by Leonard, 1 week ago</span>
-                                <img src="http://placehold.it/511x438" alt="image description">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="txt-frame wow fadeInRight" data-wow-delay="0.2s">
-                        <h2 class="tild">НАШ БЛОГ</h2>
-                        <span class="num">04</span>
-                        <a href="/blog" class="more">Больше</a>
-                    </div>
-                </div>
+                    <!-- </div> -->
+                </section>
                 </!-- price info -->
                 <div class="info price">
                     <div class="img-frame wow fadeInLeft" data-wow-delay="0.2s">
