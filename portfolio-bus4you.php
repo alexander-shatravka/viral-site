52d7fe96935f8bbb6914c020ea851fa8e3b24e4b
Indiel <kori.yanagi@gmail.com>
2019-04-01 17:22:59 +0300

    portfolio

--- portfolio-bus4you.php
+++ portfolio-bus4you.php
@@ -1,25 +1,25 @@
 <?php include 'top.php';?>
 <main id="main" role="main">
-<div class="container">
+    <div class="container">
         <div class="description-block">
             <div class="row wow fadeInRight" data-wow-delay="0.2s" >
                 <h2>Задача</h2>
                 <div class="text">
                     <p><span style="font-weight: 400;">К нам обратился клиент, занимающийся регулярными пассажирскими перевозками из Харькова в Москву и обратно, с целью увеличить количество клиентов и, соответственно, продаж. То есть нам нужно было:</span></p>
                     <ul>
-                    <li style="font-weight: 400;"><span style="font-weight: 400;">Разработать логотип, создать сайт с удобным функционалом для пользователей</span></li>
-                    <li style="font-weight: 400;"><span style="font-weight: 400;">Обеспечить ресурсу качественное отображение на любых устройствах &mdash; как десктопах, так и планшетах, смартфонах, с помощью адаптивной верстки</span></li>
-                    <li style="font-weight: 400;"><span style="font-weight: 400;">Привлечь целевую аудиторию на сайт</span></li>
-                    <li style="font-weight: 400;"><span style="font-weight: 400;">Увеличить конверсию</span></li>
+                        <li style="font-weight: 400;"><span style="font-weight: 400;">Разработать логотип, создать сайт с удобным функционалом для пользователей</span></li>
+                        <li style="font-weight: 400;"><span style="font-weight: 400;">Обеспечить ресурсу качественное отображение на любых устройствах &mdash; как десктопах, так и планшетах, смартфонах, с помощью адаптивной верстки</span></li>
+                        <li style="font-weight: 400;"><span style="font-weight: 400;">Привлечь целевую аудиторию на сайт</span></li>
+                        <li style="font-weight: 400;"><span style="font-weight: 400;">Увеличить конверсию</span></li>
                     </ul>
                     <p>&nbsp;</p>
                     <p><span style="font-weight: 400;">До обращения к нам у клиента продажи совершались в телефонном режиме.</span></p>
                     <p><span style="font-weight: 400;">К нам обратился клиент, занимающийся регулярными пассажирскими перевозками из Харькова в Москву и обратно, с целью увеличить количество клиентов и, соответственно, продаж. То есть нам нужно было:</span></p>
                     <ul>
-                    <li style="font-weight: 400;"><span style="font-weight: 400;">Разработать логотип, создать сайт с удобным функционалом для пользователей</span></li>
-                    <li style="font-weight: 400;"><span style="font-weight: 400;">Обеспечить ресурсу качественное отображение на любых устройствах &mdash; как десктопах, так и планшетах, смартфонах, с помощью адаптивной верстки</span></li>
-                    <li style="font-weight: 400;"><span style="font-weight: 400;">Привлечь целевую аудиторию на сайт</span></li>
-                    <li style="font-weight: 400;"><span style="font-weight: 400;">Увеличить конверсию</span></li>
+                        <li style="font-weight: 400;"><span style="font-weight: 400;">Разработать логотип, создать сайт с удобным функционалом для пользователей</span></li>
+                        <li style="font-weight: 400;"><span style="font-weight: 400;">Обеспечить ресурсу качественное отображение на любых устройствах &mdash; как десктопах, так и планшетах, смартфонах, с помощью адаптивной верстки</span></li>
+                        <li style="font-weight: 400;"><span style="font-weight: 400;">Привлечь целевую аудиторию на сайт</span></li>
+                        <li style="font-weight: 400;"><span style="font-weight: 400;">Увеличить конверсию</span></li>
                     </ul>
                     <p>&nbsp;</p>
                     <p><span style="font-weight: 400;">До обращения к нам у клиента продажи совершались в телефонном режиме.</span></p>
@@ -44,7 +44,7 @@
             </div>
         </div>
     </div>
-    <div class="max-container">
+    <!-- <div class="max-container">
         <div class="work-prestntation">
             <img src="images/arigato-work-1.png" alt="">
             <img src="images/arigato-work-2.png" alt="">
@@ -55,7 +55,30 @@
             <img src="images/arigato-work-thin-2.png" alt="">
             <img src="images/arigato-work-thin-3.png" alt="">
         </div>
-    </div>
+    </div> -->
+    <!-- <div class="container">
+        <div class="description-block">	
+            <div class="row wow fadeInRight" data-wow-delay="0.2s" >
+                <h2>Результат</h2>
+                <div class="text">
+                    <p>
+                        За несколько месяцев кропотливой работы рост трафика составил с 0 до 17 340 человек в месяц. Соответственно, продажи компании увеличились в 4 раза. 
+                    </p>
+                    <h3><span style="font-weight: 400;">Запуск рекламной кампании</span></h3>
+                    <p><span style="font-weight: 400;">Для увеличения конверсии, кроме поискового трафика, использовали социальные сети Facebook и Instagram. Для этого мы:</span></p>
+                    <ul>
+                    <li style="font-weight: 400;"><span style="font-weight: 400;">зарегистрировали аккаунты;</span></li>
+                    <li style="font-weight: 400;"><span style="font-weight: 400;">создали бизнес-страницы;</span></li>
+                    <li style="font-weight: 400;"><span style="font-weight: 400;">разработали контент-план на несколько месяцев;</span></li>
+                    <li style="font-weight: 400;"><span style="font-weight: 400;">наполнили страницы контентом;</span></li>
+                    <li style="font-weight: 400;"><span style="font-weight: 400;">запустили таргетированную рекламу.</span></li>
+                    </ul>
+                    <p>&nbsp;</p>
+                    <p><span style="font-weight: 400;">Старый логотип клиента мы также переделали, сделав его более запоминающимся и узнаваемым.</span></p>
+                </div>
+            </div>
+        </div>
+    </div> -->
     </div>
 </main>
 <?php include 'bottom.php';?>
